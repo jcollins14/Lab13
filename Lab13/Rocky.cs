d3b03a0efd39e40aa024b6759bcccc3dcf814b21
Jake Collins <jakecollins1014@gmail.com>
2020-02-03 23:33:43 -0500

    updated rocky

--- Lab13/Rocky.cs
+++ Lab13/Rocky.cs
@@ -6,6 +6,15 @@ namespace Lab13
 {
     class Rocky : Player
     {
+        public Rocky(string name)
+        {
+            this.Name = name;
+            this.Action = Roshambo.rock;
+        }
 
+        protected override Roshambo generateRoshambo()
+        {
+            return Roshambo.rock;
+        }
     }
 }
